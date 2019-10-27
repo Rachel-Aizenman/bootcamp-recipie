39cab8792aaaa12e8cf3e099955650983f7ccc17
Nitzancohen <orhirak@gmail.com>
2019-10-27 14:16:08 +0200

    final

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -1,10 +1,3 @@
-// const render = function(data){
-//     const source = $("#recipe-template").html()
-//     const template = Handlebars.compile(source)
-//     const newHTML = template({data})
-//     $("#display-recipe").append(newHTML)
-// }
-
 class Renderer{
     constructor(){
     }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 8080
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))//===========================================//
+app.use(express.static(path.join(__dirname, 'node_modules')))
 
 app.use('/sanity', (req, res)=>{
     res.send("OK")
